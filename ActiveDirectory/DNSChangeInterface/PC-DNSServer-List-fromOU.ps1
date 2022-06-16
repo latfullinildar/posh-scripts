f93b9e5ada39e9f90dddb0dfe3da482ca6a6f6e3
Ildar Latfullin <admmarker@yandex.ru>
2022-06-16 14:26:50 +0300

    Added AD scripts:DNSChnageInterface

---
+++ ActiveDirectory/DNSChangeInterface/PC-DNSServer-List-fromOU.ps1
@@ -0,0 +1,15 @@
+﻿#Скрипт для определения DNS сетевых интерфейсов
+# Импортируем модуль Active Directory
+Import-Module ActiveDirectory
+
+#Вводим область поиска, или DistinguisgedName
+$sb = "OU=computers,DC=example,DC=corp"
+
+#Можем взять наименование компьюетров из списка
+#$pcs = Import-Csv $PSScriptRoot\pcname.csv
+
+
+$pcs = Get-ADComputer -Filter {ObjectClass -eq "Computer"} -SearchBase $sb -SearchScope Subtree
+Foreach($pc in $pcs){
+  Get-CimInstance -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=$true -ComputerName $pc.Name |Select-Object -Property DNSServerSearchOrder,DNShostname,DHCPEnabled
+ }#Foreach
---
+++ ActiveDirectory/DNSChangeInterface/PC-DNSServerSearchOrder-Set-fromOU.ps1
@@ -0,0 +1,26 @@
+﻿#Скрипт для изменения DNS на удаленных компьютерах
+#Импортируем модуль Active Directory
+Import-Module ActiveDirectory
+
+#Вводим область поиска, или DistinguisgedName
+$sb = "OU=test,DC=example,DC=corp"
+
+#Ищем компьютеры в AD
+$pcs = Get-ADComputer -Filter {ObjectClass -eq "Computer"} -SearchBase $sb -SearchScope Subtree
+
+#Можем взять наименование компьютеров из списка
+#$pcs = Import-Csv $PSScriptRoot\pcname.csv
+
+ForEach ($pc in $pcs) {
+    Write-Host "Computer $($pc.Name)"
+    Invoke-Command -ComputerName $pc.Name -ScriptBlock {
+        $NewDnsServerSearchOrder = "10.101.101.10","10.101.101.51"
+        $Adapters = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.DHCPEnabled -ne 'True' -and $_.DNSServerSearchOrder -ne $null}
+        Write-Host "Old DNS: "
+        $Adapters | ForEach-Object {$_.DNSServerSearchOrder}
+        $Adapters | ForEach-Object {$_.SetDNSServerSearchOrder($NewDnsServerSearchOrder)} | Out-Null
+        $Adapters = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.DHCPEnabled -ne 'True' -and $_.DNSServerSearchOrder -ne $null}
+        Write-Host "New DNS: "
+        $Adapters | ForEach-Object {$_.DNSServerSearchOrder}
+        }
+}
