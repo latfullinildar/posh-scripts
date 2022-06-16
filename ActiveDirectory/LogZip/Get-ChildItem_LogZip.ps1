1b5e0daf40c6a87111349982f98f5f508dec2d4c
Ildar Latfullin <admmarker@yandex.ru>
2022-06-16 15:00:27 +0300

    Added AD scripts:LogsZip

---
+++ ActiveDirectory/LogZip/Get-ChildItem_LogZip.ps1
@@ -0,0 +1,29 @@
+﻿#Читай README
+#каталог с логами
+$logFolder = "C:\Logs\*"
+$include=@("*.zip")
+#ищем логи, с датой старше 1 дня и больше 30 мб
+$logs = Get-ChildItem -Path $logFolder -Recurse -Include *.log | Where-Object -FilterScript {($_.LastWriteTime -lt (Get-Date).adddays(-1)) -and ($_.Length -ge 30mb)}
+#сжимаем найденные логи и удаляем файл с расширением .log
+foreach ($log in $logs) {
+    #$logfullname = $log.FullName
+    #$logname = $log.Name
+    #$log.FullName
+    try {
+            $lfname = $log.FullName
+            $lname = $log.Name
+            $compress = @{
+            LiteralPath = "$lfname"
+            CompressionLevel = "Fastest"
+            DestinationPath = "C:\Logs\$lname.zip"
+            }
+        Compress-Archive @compress
+        Remove-Item $lfname
+    }
+    catch{
+        Write-error $($_.Exception.message)
+    }
+}
+# удаляем старые логи, больше дней указанных в переменной, и имя не содержит слово "archive"
+$cutoffDays = 30
+Get-ChildItem -Path $logFolder -Include $include | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(0 - $cutoffDays) -and $_.Name -notmatch "archive" } | Remove-Item -WhatIf
---
+++ ActiveDirectory/LogZip/RemotePC-ScriptBlock.ps1
@@ -0,0 +1,13 @@
+#Удаленной выполнение скриптов
+$Dcs = Import-CSV "$PSScriptRoot\dc.txt"
+
+ForEach ($dc in $Dcs) {
+     
+    try {
+        #Invoke-Command -ComputerName $dc.Name -ScriptBlock {Get-UICulture}
+        Invoke-Command -ComputerName $dc.Name -FilePath "$PSScriptRoot\Get-ChildItem_LogZip.ps1"
+    }
+    catch{
+    Write-error $($_.Exception.message)
+    }
+}
